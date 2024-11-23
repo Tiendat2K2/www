d088d2edc2262ce464c3620db8beda2ca3207054
Tiendat2K2 <109364019+Tiendat2K2@users.noreply.github.com>
2024-11-23 09:31:08 +0700

    Update Teacher.Header.js
    1

--- src/components/Teacher/Header/Teacher.Header.js
+++ src/components/Teacher/Header/Teacher.Header.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Layout, Avatar, Menu, Dropdown, Modal, Form, Input, Button, message, Upload ,Select} from 'antd';
 import { UserOutlined, LogoutOutlined, LockOutlined, UploadOutlined } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import Logo from '../../../assets/img/logo.png';
 import { API_URL } from '../../../utils/api';
 import axios from 'axios';
@@ -162,7 +162,7 @@ const CustomHeader = () => {
   return (
     <>
       <Header style={headerStyle}>
-        <img src={Logo} alt="Logo" style={{ height: '40px' }} />
+       <Link to="/Teacher"><img src={Logo} alt="Logo" style={{ height: '40px' }} /></Link>
         <Dropdown overlay={menu} trigger={['click']} placement="bottomRight">
           <Avatar icon={<UserOutlined />} style={{ backgroundColor: '#87d068', cursor: 'pointer' }} />
         </Dropdown>
